809893f7c96ead8dd3427f3f896f8ed2469fc75e
ali <1989hossaini1@gmail.com>
2026-01-12 19:23:31 +0100

    added script ci in pakage json

--- src/app/posts/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-// import { fetchPosts } from "@/actions/FetchPost"
-
-// export async function generateStaticParams() {
-//   const posts = await fetchPosts({ limit: 1000 }) // oder pagination false, wenn du willst
-//   return posts.map((post) => ({ slug: post.slug }))
-// }
